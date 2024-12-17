52f8c329ccfc5d72ded0ccfec2675e9518027739
DavidBalishyan <davidbalishyan12@gmail.com>
2024-12-17 16:43:04 +0400

    LrnMore page needs to be done

--- src/pages/LrnMore.jsx
+++ src/pages/LrnMore.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
+
 const LrnMore = () => {
     return (
         <div>
             <h1>
-                Hello ||  Learn More!
+                Hello ||  Learn More! page
             </h1>
+            <Link to="/" className="btn btn-primary">/</Link>
         </div>
     )
 }
